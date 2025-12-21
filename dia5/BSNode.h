a4628227116c778874cee3d3cc6ecc4be1165070
Àngel Chofre Ortega <achoort1@etsinf.upv.es>
2025-12-21 22:25:52 +0100

    Update BSNode.h

--- dia5/BSNode.h
+++ dia5/BSNode.h
@@ -10,12 +10,14 @@ public:
     BSNode<T>* left;
     BSNode<T>* right;
 
+    // Crea un nodo con un dato y sus hijos
     BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr) {
         this->elem = elem;
         this->left = left;
         this->right = right;
     }
 
+    // Permite imprimir el nodo con cout
     friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn) {
         out << bsn.elem;
         return out;
